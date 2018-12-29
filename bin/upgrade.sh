5604ef5762c4f6f0f6e8238469e5df0232b477f7
Sandro Lutz <code@temparus.ch>
2018-12-29 11:22:34 +0100

    Update upgrade.sh

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -10,8 +10,11 @@ layman -S
 # Upgrade every additional package installed on the system
 emerge -avuDN --with-bdeps y --keep-going world
 
-# Update configuration files
-etc-update
+# Apply updates to the configuration files and try again.
+if [ $? -eq 1 ]; then
+  etc-update --automode -5
+  emerge -avuDN --with-bdeps y --keep-going world
+fi
 
 # Removes unused dependencies
 emerge -av --depclean
