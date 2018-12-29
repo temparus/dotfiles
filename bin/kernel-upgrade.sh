8d2fa44d5581a6dc8fb7653855f65e336cbe1d00
Sandro Lutz <code@temparus.ch>
2018-12-29 11:34:07 +0100

    Update kernel-upgrade.sh

--- bin/kernel-upgrade.sh
+++ bin/kernel-upgrade.sh
@@ -6,30 +6,46 @@ CURRENT_KV=$(uname -r)
 DEVICE_NAME=$(uname -n)
 NEW_KV=$(basename `realpath /usr/src/linux`)
 
-# This assumes that the kernel ebuild was emerged with the
-# 'symlink' use flag set, so that /usr/src/linux points to the
-# newly emerged kernel.
-cp "/usr/src/linux-${CURRENT_KV}/.config" "/usr/src/linux/"
+if [[ $CURRENT_KV = $NEW_KV ]]; then
+  echo "Already on Latest Version"
+  read -r -p "Run menuconfig? [Y/n] " response
+  response=${response,,} # tolower
+  if ! [[ $response =~ ^(no|n)$ ]]; then
+    ( cd $path && make menuconfig )
+  fi
+else
+  echo "Performing Upgrade $CURRENT_KV -> $NEW_KV"
 
-cd "/usr/src/linux"
-make olddefconfig
+  # This assumes that the kernel ebuild was emerged with the
+  # 'symlink' use flag set, so that /usr/src/linux points to the
+  # newly emerged kernel.
+  cp "/usr/src/linux-${CURRENT_KV}/.config" "/usr/src/linux/"
 
-# Make a copy for the dotfiles repository
-cp "/usr/src/linux/.config" "/home/${USER}/.config/kernel/${DEVICE_NAME}.config"
-chown "${USER}" "/home/${USER}/.config/kernel/${DEVICE_NAME}.config"
-chgrp "${USER}" "/home/${USER}/.config/kernel/${DEVICE_NAME}.config"
+  cd "/usr/src/linux"
+  make olddefconfig
 
-cd "/home/${USER}"
+  # Make a copy for the dotfiles repository
+  cp "/usr/src/linux/.config" "/home/${USER}/.config/kernel/${DEVICE_NAME}.config"
+  chown "${USER}" "/home/${USER}/.config/kernel/${DEVICE_NAME}.config"
+  chgrp "${USER}" "/home/${USER}/.config/kernel/${DEVICE_NAME}.config"
 
-alias dots="git --git-dir=/home/${USER}/.dots.git/ --work-tree=/home/${USER}/"
-dots add -f ".config/kernel/${DEVICE_NAME}.config"
-dots commit --author "${AUTHOR}" -m "[${DEVICE_NAME}] Update kernel config to ${NEW_KV}"
-dots push
+  cd "/home/${USER}"
+
+  alias dots="git --git-dir=/home/${USER}/.dots.git/ --work-tree=/home/${USER}/"
+  dots add -f ".config/kernel/${DEVICE_NAME}.config"
+  dots commit --author "${AUTHOR}" -m "[${DEVICE_NAME}] Update kernel config to ${NEW_KV}"
+  dots push
+fi
+
+echo "Building kernel $NEW_KV"
 
 cd /usr/src/linux
 
 make -j5 && make modules_install
 mount /boot
+
+echo "Installing kernel $NEW_KV"
+
 make install
 # only keep the newest three kernels
 eclean-kernel -n 3
