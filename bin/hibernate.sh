0d73aad35818f17f4d2ed7d0b329a01b1fd7ec7d
Sandro Lutz <code@temparus.ch>
2019-01-16 17:56:23 +0100

    Fix issues with suspend2ram (immediate wakeup)

--- bin/hibernate.sh
+++ bin/hibernate.sh
@@ -2,5 +2,17 @@
 
 script_dir=$(dirname $0)
 
+function disable_acpi_wakeup {
+  ACTIVE=$(grep enabled /proc/acpi/wakeup | cut -f 1 | grep "$1")
+  if [ -n "$ACTIVE" ]; then 
+    sudo su -c "echo \"$ACTIVE\" > /proc/acpi/wakeup"
+    echo "DEBUG: disabled -$ACTIVE-" >&2
+  else
+    echo "DEBUG: no active -$1- found." >&2
+  fi
+}
+
+disable_acpi_wakeup XHC
+
 $script_dir/lock.sh && sudo s2ram
 
